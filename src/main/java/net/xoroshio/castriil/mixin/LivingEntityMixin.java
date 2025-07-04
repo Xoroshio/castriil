e71849438d13aa91ce9d2705c17325de20cb3c78
Xoroshio <foofamily41@gmail.com>
2025-07-04 17:15:44 +1000

    Main

--- src/main/java/net/xoroshio/castriil/mixin/LivingEntityMixin.java
+++ src/main/java/net/xoroshio/castriil/mixin/LivingEntityMixin.java
@@ -1,4 +1,19 @@
 package net.xoroshio.castriil.mixin;
 
+import net.minecraft.core.Holder;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
 public class LivingEntityMixin {
+
+    @Inject(at = @At("RETURN"), method = "hasEffect", cancellable = true)
+    public void hasEffect(Holder<MobEffect> effect, CallbackInfoReturnable<Boolean> callback){
+        callback.setReturnValue(callback.getReturnValue() || effect == MobEffects.NIGHT_VISION);
+    }
 }
